0939c01548aa1cc2f33220069048cd0615f50e08
edjuarezp <edjuarezp@L01452763M01.local>
2026-02-11 13:06:04 -0600

    archivo de preparación de sesión de clase

---
+++ intro.js
@@ -0,0 +1,208 @@
+// consola (log, info, warn, error, assert)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// variables, constantes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Alcance de las variables
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// alert, prompt, confirm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// funciones tradicionales
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// funciones modernas
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// arreglos
+
+
+//const arreglo = ["Elemento"];
+
+
+//const arreglo = new Array() 
+
+
+//arreglo.push("Otro elemento");
+
+
+//arreglo[10] = "Uno más";
+
+
+//arreglos asociativos
+
+
+//recorrido tradicional del arreglo
+
+
+//for (let i = 0; i < arreglo.length; i++) {
+
+
+//    console.log(arreglo[i]);
+
+
+//}
+
+
+//recorridos alternativos del arreglo
+
+
+//for(let posicion in arreglo) {
+
+
+//    console.log(posicion);
+
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Objetos
+
+
+//const objeto = {} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// modificar html
