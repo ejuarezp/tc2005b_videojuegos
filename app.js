ea722ac3e569974062570c81f83c9c57470873cf
edjuarezp <edjuarezp@L01452763M01.local>
2026-02-18 09:48:16 -0600

    archivo de preparación para la sesión de node

---
+++ app.js
@@ -0,0 +1,41 @@
+//const filesystem = require('fs');
+
+//filesystem.writeFileSync('hola.txt', 'Hola desde node');
+
+
+
+
+
+
+
+
+
+
+
+// const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+
+// for (let item of arreglo) {
+//     setTimeout(() => {
+//         console.log(item);
+//     }, item);
+// } 
+
+
+
+
+
+
+
+
+
+
+// const http = require('http');
+
+// const server = http.createServer( (request, response) => {    
+//     console.log(request.url);
+//     response.setHeader('Content-Type', 'text/html');
+//     response.write("");
+//     response.end();
+// });
+
+// server.listen(3000);
