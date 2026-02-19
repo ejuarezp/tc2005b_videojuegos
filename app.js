2f978fbcf9c843ffbd3216db7e3a0bdeff60c232
edjuarezp <edjuarezp@L01452763M01.local>
2026-02-19 10:13:58 -0600

    imprimir números en orden con funciones asincrónicas

--- app.js
+++ app.js
@@ -5,22 +5,13 @@ const filesystem = require('fs');
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
 
 
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
 
-
-
-
-
-
-
-
-
-// const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
-
-// for (let item of arreglo) {
-//     setTimeout(() => {
-//         console.log(item);
-//     }, item);
-// } 
+for (let item of arreglo) {
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+} 
 
 
 
