2d5cec5d6b787ee42d093c2a924fffeac2a8b9e0
edjuarezp <edjuarezp@L01452763M01.local>
2026-02-23 09:46:16 -0600

    cleanup para lab 10

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
-console.log("hola desde node!");
-
-const filesystem = require('fs');
-
-filesystem.writeFileSync('hola.txt', 'Hola desde node');
-
-setTimeout(() => {
-    console.log("jojo te hackié");
-}, 15000);
-
-const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
-
-for (let item of arreglo) {
-    setTimeout(() => {
-        console.log(item);
-    }, item);
-} 
+const http = require('http');
 
 const html = `
 <!DOCTYPE html>
@@ -86,13 +70,9 @@ const html = `
 </html>    
 `;
 
-
-const http = require('http');
-
 const server = http.createServer( (request, response) => {    
-//    console.log(request);
+
     console.log(request.url);
-//   console.log(response);
     response.setHeader('Content-Type', 'text/html');
     response.write(html);
     response.end();
