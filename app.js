9eceed1ae73c90d27b38fd69e4e4685a57d00119
edjuarezp <edjuarezp@L01452763M01.local>
2026-02-24 10:37:00 -0600

    atender peticiones por get y post y guardar los datos por medio de request.body

--- app.js
+++ app.js
@@ -69,7 +69,12 @@ app.use((request, response, next) => {
     next(); //Le permite a la petición avanzar hacia el siguiente middleware
 });
 
-app.use('/new', (request, response, next) => {
+app.get('/new', (request, response, next) => {
+    response.send(html_header + html_form + html_footer);
+});
+
+app.post('/new', (request, response, next) => {
+    videojuegos.push(request.body);
     response.send(html_header + html_form + html_footer);
 });
 
